004751f1c66cede4d84101dc5939533c615766d2
Norah A Miller <mill2439@data.cs.purdue.edu>
2021-09-17 21:02:16 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -3,43 +3,62 @@
 # Changes to the ORIGIN directory
 cd ORIGIN
 
-# Task 1 
+# Task 1
+
+mv Homework/MA165/HW2.txt Homework/MA165/HW2.txt
 
 
 # Task 2
 
+rm Memes/Spongebob/money.jpeg
 
 # Task 3
 
+mv Memes/StarWars/fafsa.jpg Memes/SpongeBob
 
 # Task 4
 
+rm -r  Homework/CS191
 
 # Task 5
 
+mkdir Programs/C++
 
 # Task 6
 
+mv Programs/Age.cpp Programs/C++
 
 # Task 7
 
+cp Programs/Java Homework/CS180
+mv Homework/CS180/Java Homework/CS180/Project-1
 
 # Task 8
 
+mv Programs/Count.c Programs/C
+mv Programs/Goodbye.c Programs/C
 
 # Task 9
 
+mv Programs/Name.py Programs/Python3
+mv Programs/Space.py Programs/Python3
 
 # Task 10
 
+touch Homework/POL141/Essay.txt
 
 # Task 11
 
+ls
 
 # Task 12
+
 : '
 Type your vim/nano customizations below:
 
+:set number
+:set clipboard=unnamed,unnamedplus
+::set mouse=a
 
 
 '
